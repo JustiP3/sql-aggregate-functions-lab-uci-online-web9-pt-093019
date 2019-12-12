a93e9119a3c54f4ff1bef27174166f51d52f2868
Justin Phillips <justinphil02@yahoo.com>
2019-12-12 20:10:59 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-  "sqlite3 students.db;" 
+  "sqlite3 students.db;"
   "SELECT MAX(gpa) FROM students;"
 end
 
